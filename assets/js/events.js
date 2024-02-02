8be22ec4942eda66fc69539350729217fa0862e6
jaimesilvalorca <jaimesilvalorca@gmail.com>
2024-02-02 10:40:43 -0300

    eventos agregados

---
+++ assets/js/events.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    console.log("documento cargado")
+    $("a").on('click', function(event) {
+
+      if (this.hash !== "") {
+
+        event.preventDefault();
+
+        var hash = this.hash;
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+          window.location.hash = hash;
+        });
+      }
+    });
+
+    $('#enviarContacto').click(function(){
+        alert("Datos enviados!!"  )
+    })
+  });
